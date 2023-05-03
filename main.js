b4e4f6dbf0191084f95feb927778df92debab5b8
simple-sifu <kattyntommy@gmail.com>
2023-05-03 19:27:02 -0400

    added examples

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ const buildSandwich = (ingredient1) => {
 
 const mySandwich = buildSandwich('ham')('cheese')('mustard');
 console.log(mySandwich);
+
+// it works but thats getting ugly and nested the further we go
+// let's refactor
+
+const buildSammy = ingred1 => ingred2 => ingred3 => `${ingred1} and ${ingred2} and ${ingred3} sandwich`;
+
+const mySammy = buildSammy('ham')('cheese')('mustard');
+console.log(mySammy);
+
+
+// Another example of a curried function
+const multiply = (x, y) => x * y;
+
+
+const curriedMultiply = x => y => x * y;
+
+console.log(multiply(2, 3));
+console.log(curriedMultiply(2)(3));
+
+
